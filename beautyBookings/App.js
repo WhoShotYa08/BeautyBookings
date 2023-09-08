bd84f1415e504d4aeca42a9ea3517f3b496a0433
WhoShotYa08 <80717684+WhoShotYa08@users.noreply.github.com>
2023-09-09 00:27:35 +0200

    added the expo/vector-icons dependencies

--- beautyBookings/App.js
+++ beautyBookings/App.js
@@ -2,16 +2,34 @@ import { StyleSheet, Text, View } from 'react-native';
 import { HomeScreen, ProfileScreen } from './src/screens';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from "@react-navigation/native";
-
+import { Entypo } from ""
 const Tab = createBottomTabNavigator();
 
-
+const screenOptions = {
+  tabBarShowLabel: false,
+  HeaderShown: false,
+  tabBarStyle:{
+    postion: 'absolute',
+    bottom: 0,
+    right: 0,
+    left: 0,
+    elevation: 0,
+    height: 60,
+    backgroundColor: '#fff'
+  }
+}
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen}/>
+        <Tab.Screen 
+          name="Home"
+          component={HomeScreen}
+          options={(focused)=>{
+
+          }} 
+        />
         <Tab.Screen name="Profile" component={ProfileScreen}/>
       </Tab.Navigator>
     </NavigationContainer>
