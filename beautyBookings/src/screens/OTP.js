2f52ad883628f51076403645d1d2ee1fd7af339a
WhoShotYa08 <80717684+WhoShotYa08@users.noreply.github.com>
2023-10-03 07:52:12 +0200

    giving the Otp styling

--- beautyBookings/src/screens/OTP.js
+++ beautyBookings/src/screens/OTP.js
@@ -1,16 +1,78 @@
 import { useState } from "react";
 import { Text, StyleSheet, SafeAreaView, TouchableOpacity, View} from 'react-native';
 import { TextInput } from "react-native-gesture-handler";
-
+import sty from "./Style";
+import GradientCircle from "../components/GradientCircle";
 
 export default function OtpScreen(){
+    const [one, setOne] = useState();
+    const [two, setTwo] = useState();
+    const [three, setThree] = useState();
+    const [four, setFour] = useState();
+    const [five, setFive] = useState();
+    const [six, setSix] = useState();
+
+
+
     return(
-        <SafeAreaView style={styles.main}>
-            <View>
-                <TextInput 
-                    style={styles.inputBox}
-                />
+        <SafeAreaView style={{flex: 1}}>
+            <Text style={[sty.header, {textAlign: 'center', padding: 15}]}>Confirmation</Text>
+
+            <View style={styles.main}>
+                <Text>Enter code sent to</Text>
+                <View style={{flexDirection: 'row'}}>
+                    <TextInput 
+                        style={styles.inputBox}
+                        keyboardType="number-pad"
+                        numberOfLines={1}
+                        maxLength={1}
+                        onChangeText={(t)=>setOne(t)}
+                    />
+                    <TextInput 
+                        style={styles.inputBox}
+                        keyboardType="number-pad"
+                        numberOfLines={1}
+                        maxLength={1}
+                        onChangeText={(t)=>setTwo(t)}
+                    />
+                    <TextInput 
+                        style={styles.inputBox}
+                        keyboardType="number-pad"
+                        numberOfLines={1}
+                        maxLength={1}
+                        onChangeText={(t)=>setThree(t)}
+                    />
+                    <TextInput 
+                        style={styles.inputBox}
+                        keyboardType="number-pad"
+                        numberOfLines={1}
+                        maxLength={1}
+                        onChangeText={(t)=>setFour(t)}
+                    />
+                    <TextInput 
+                        style={styles.inputBox}
+                        keyboardType="number-pad"
+                        numberOfLines={1}
+                        maxLength={1}
+                        onChangeText={(t)=>setFive(t)}
+                    />
+                    <TextInput 
+                        style={styles.inputBox}
+                        keyboardType="number-pad"
+                        numberOfLines={1}
+                        maxLength={1}
+                        onChangeText={(t)=>setSix(t)}
+                    />
+                </View>
+
+                <TouchableOpacity>
+                    <Text style={{fontSize: 19, marginVertical: 20, borderWidth: 1, padding: 10}}>
+                        Verify
+                    </Text>
+                </TouchableOpacity>
+
             </View>
+            {/* <GradientCircle/> */}
         </SafeAreaView>
     )
 }
@@ -20,9 +82,16 @@ const styles = StyleSheet.create({
     main: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+    
     },
     inputBox: {
-        borderWidth: 1
-    }
+        borderWidth: 1,
+        padding: 10,
+        marginHorizontal: 3,
+        textAlign: 'center',
+        marginVertical: 10
+    },
+
+    
 })
