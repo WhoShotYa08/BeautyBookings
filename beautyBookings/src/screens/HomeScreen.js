f1e46ed2828a01adc81bb04858094f12d74cb6e7
WhoShotYa08 <80717684+WhoShotYa08@users.noreply.github.com>
2023-10-24 07:02:41 +0200

    reshaffuled a lot of code

--- beautyBookings/src/screens/HomeScreen.js
+++ beautyBookings/src/screens/HomeScreen.js
@@ -1,77 +1,22 @@
 import { SafeAreaView, Text, TextInput, View, TouchableOpacity, ImageBackground } from "react-native";
 import Icon from 'react-native-vector-icons/AntDesign';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Style";
 import { ScrollView } from "react-native-gesture-handler";
-import { collection, where, getDocs, query, doc } from 'firebase/firestore';
+import { collection, getDocs, query,} from 'firebase/firestore';
 import { db } from "../components/db/firebase_";
-import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 
 
-
-const businesses = [
-    {
-        name: "GGS New Star Hair & Beauty Saloon",
-        location: "Alberton",
-        time: "08:00 - 20:00",
-        imgLink: require("../images/salon1.jpg"),
-        rating: 4,
-        contact: "068 296 0139",
-    },
-    {
-        name: "Gugu Hair Saloon",
-        location: "Johannesburg",
-        time: "08:00 - 16:00",
-        imgLink: require("../images/salon2.jpg"),
-        rating: 2,
-        contact: "068 296 0139",
-    },
-    {
-        name: "sun shine barber & beauty saloon",
-        location: "Alberton",
-        time: "08:00 - 20:00",
-        imgLink: require("../images/salon1.jpg"),
-        rating: 5,
-        contact: "068 296 0139",
-    },
-    {
-        name: "JL GLAM STRANDS AND BEAUTY",
-        location: "Alberton",
-        time: "08:00 - 20:00",
-        imgLink: require("../images/salon4.jpg"),
-        rating: 4,
-        contact: "068 296 0139",
-    },
-    {
-        name: "GGS New Star Hair & Beauty Saloon",
-        location: "Alberton",
-        time: "08:00 - 20:00",
-        imgLink: require("../images/salon1.jpg"),
-        rating: 4,
-        contact: "068 296 0139",
-    },
-    {
-        name: "GGS New Star Hair & Beauty Saloon",
-        location: "Alberton",
-        time: "08:00 - 20:00",
-        imgLink: require("../images/salon5.jpg"),
-        rating: 4,
-        contact: "068 296 0139",
-    },
-
-]
-
-
-function SaloonDetials({imgLink, name, address, workingHours}){
+function SaloonDetials({imgLink, name, address, workingHours, rating}){
     const [liked, setLiked] = useState(false)
 
     const icon = liked? "heart": "hearto";
     const heartColor = liked? 'red': '#fff';
 
     return(
-        <View style={{flexDirection: 'row', borderBottomWidth: 1, padding: 10}}>
-            <View>
-                <ImageBackground source={imgLink} 
+        <View style={{flexDirection: 'row', borderBottomWidth: 1, padding: 10, width: '100%', flexWrap: 'wrap'}}>
+            <View style={{flex: 1}}>
+                <ImageBackground source={{uri:imgLink}} 
                     style={{height: 125, width: 125, marginRight: 10, alignItems: 'flex-end', justifyContent:'flex-end', padding: 7}}
                 >
                     <TouchableOpacity onPress={()=>setLiked(!liked)}>
@@ -80,14 +25,13 @@ function SaloonDetials({imgLink, name, address, workingHours}){
                 </ImageBackground>
             </View>
 
-            <View style={{flexWrap: 'wrap'}}>
-                <Text style={{fontWeight: '700', fontSize: 16, flexWrap: 'wrap',}}>{name}</Text>
+            <View style={{flex: 2, paddingHorizontal: 20, marginLeft: 15}}>
+                <Text style={{fontWeight: '700', fontSize: 21, flexWrap: 'wrap'}}>{name}</Text>
                 <Text>{address}</Text>
-                <Text>{workingHours}</Text>
+                <Text style={{color: 'lightgrey'}}>{workingHours}</Text>
 
-                <View style={{flexDirection: 'row'}}>
+                <Stars rating={rating}/>
 
-                </View>
             </View>
 
         </View>
@@ -95,21 +39,80 @@ function SaloonDetials({imgLink, name, address, workingHours}){
 }
 
 
-export default function HomeScreen({navigation}){
+function Stars({rating}){
+    switch (rating){
+        case 1:
+            return(
+                <View style={{flexDirection: 'row'}}>
+                    <Icon name="star" size={24} color={'orange'}/>
+                    <Icon name="staro" size={24}/>
+                    <Icon name="staro" size={24}/>
+                    <Icon name="staro" size={24}/>
+                    <Icon name="staro" size={24}/>
+                </View>
+            )
+        case 2:
+            return(
+                <View style={{flexDirection: 'row'}}>
+                    <Icon name="star" size={24} color={'orange'}/>
+                    <Icon name="star" size={24} color={'orange'}/>
+                    <Icon name="staro" size={24}/>
+                    <Icon name="staro" size={24}/>
+                    <Icon name="staro" size={24}/>
+                </View>
+            )
+        case 3:
+            return(
+                <View style={{flexDirection: 'row'}}>
+                    <Icon name="star" size={24} color={'orange'}/>
+                    <Icon name="star" size={24} color={'orange'}/>
+                    <Icon name="star" size={24} color={'orange'}/>
+                    <Icon name="staro" size={24}/>
+                    <Icon name="staro" size={24}/>
+                </View>
+            )
+        case 4:
+            return(
+                <View style={{flexDirection: 'row'}}>
+                    <Icon name="star" size={24} color={'orange'}/>
+                    <Icon name="star" size={24} color={'orange'}/>
+                    <Icon name="star" size={24} color={'orange'}/>
+                    <Icon name="star" size={24} color={'orange'}/>
+                    <Icon name="staro" size={24}/>
+                </View>
+            )
+        case 5:
+            return(
+                <View style={{flexDirection: 'row'}}>
+                    <Icon name="star" size={24} color={'orange'}/>
+                    <Icon name="star" size={24} color={'orange'}/>
+                    <Icon name="star" size={24} color={'orange'}/>
+                    <Icon name="star" size={24} color={'orange'}/>
+                    <Icon name="star" size={24} color={'orange'}/>
+                </View>
+            )
+        default:
+            return(
+                <Text>No Rating</Text>
+            )
+    }
+}
 
-    const [salonList, setSalonList] = useState([]);
+
+
+export default function HomeScreen(){
 
     async function GetDetails(){
         const saloonRef =  query(collection( db, "salonDetails"));
         const saloonSnap = await getDocs(saloonRef);
-
+        setSalonList([]);
         saloonSnap.forEach((doc)=>{
-            setSalonList((prev)=>[...prev, doc.data["details"]]);
-            console.log(salonList.length)
+            setSalonList((prev)=>[...prev, doc.data()]);
         })
     }
+    const [salonList, setSalonList] = useState([]);
 
-    setTimeout(GetDetails,1000);
+    useEffect(()=>{GetDetails()},[])
     const [word, setWord]= useState('');
     return(
         <SafeAreaView style={{flex: 1}}>
@@ -137,28 +140,28 @@ export default function HomeScreen({navigation}){
             </View>
 
             <Text style={{fontSize: 27, fontWeight:'600', textAlign: 'center'}}>Registered Businesses</Text>
-            <Btn text={"Chat"} func={ () =>navigation.navigate("Chat") } />
+
             <ScrollView>
                 {
                     salonList.map((item, idx)=>{
-                        let name = item.name.toLowerCase();
 
-                        if (name.includes(word.toLowerCase())){
-                            return(
-                                <TouchableOpacity key={idx} style={{marginBottom: 10}}>
-                                    <SaloonDetials 
-                                        imgLink={item.imgLink}
-                                        name={item.name}
-                                        address={item.location}
-                                        workingHours={item.time}
-                                    />
-                                </TouchableOpacity>
-                            )
-                        }
+                        let name = item['details'].name;
+
+                        return(
+                            <TouchableOpacity style={{flexWrap: 'wrap'}} key={idx}>
+                                <SaloonDetials
+                                    imgLink={item['image']}
+                                    name={item['details'].name}
+                                    address={item['details'].address}
+                                    workingHours={item['details'].workingHours}
+                                    rating={parseInt(item['details'].rating)}
+                                />
+                            </TouchableOpacity>
+                        )
+                        // let name = item.name.toLowerCase();
 
                     })
                 }
-                
             </ScrollView>
 
         </SafeAreaView>
